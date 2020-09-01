8d65c8ffbb96c5c32458cbabaeb0fef47a646ca3
Noah Scala <nwscala@gmail.com>
2020-09-01 18:31:23 -0400

    added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Image.create(link: "https://i.ytimg.com/vi/qnkMobV-GvU/maxresdefault.jpg")
 Image.create(link: "https://www.thesprucepets.com/thmb/Os5XrkxEXHrvhqeyQ66TVugXYRg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/GettyImages-522664558-58a6e85e3df78c345b61ff63.jpg")
+Image.create(link: "https://www.thesprucepets.com/thmb/7cJw7135bTLj9Gz520SJBJMgzpU=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/white-cockatoo-144761817-5c62f84846e0fb0001f08faf.jpg")
+Image.create(link: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Eolophus_roseicapilla_-Wamboin%2C_NSW%2C_Australia_-adult-8-2cp.jpg/800px-Eolophus_roseicapilla_-Wamboin%2C_NSW%2C_Australia_-adult-8-2cp.jpg")
